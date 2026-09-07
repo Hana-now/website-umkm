fd44ee3c5afb6a6a9d3445cb994edfc2eccb194b
Hana-now <hnayss572@gmail.com>
2026-09-07 10:38:09 +0700

    feat: buat lading page awal UMKM

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", function() {
+    promoButton.textContent = "Promo: Beli 2 Gratis 1!";
+    console.log("Promo Kopi Nusa berhasil ditampilkan!");
+});
